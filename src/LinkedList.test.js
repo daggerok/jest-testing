d968ecad5aebaf814592358610f0cc3212dd8616
Maksim Kostromin <daggerok@gmail.com>
2021-06-05 00:17:21 +0300

    Done. | hey, look over there! (C) whatthecommit.com :)

--- src/LinkedList.test.js
+++ src/LinkedList.test.js
@@ -1,3 +1,15 @@
+import LinkedList from './LinkedList';
+
 describe('Linked list', () => {
-  test.todo('should create empty list by default');
+
+  test('should has null next reference in empty list', () => {
+    let list = new LinkedList();
+    expect(list.next).toBeDefined()
+    expect(list.next).toBeNull()
+  });
+
+  test('should not has next on empty list', () => {
+    let list = new LinkedList();
+    expect(list.hasNext()).toBe(false)
+  });
 });
